63fdfe6675a8baa120a8e9809d2b6b749be8da58
beccagorton182 <rebecca.gorton1@nhs.net>
2020-04-16 16:30:31 +0100

    Added version control and updates for version 2

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 // Branching example
-router.post('/crisis-support-finder/finder/postcode-search-branch', function (req, res) {
+router.post('/crisis-support-finder/v2/finder/postcode-search-branch', function (req, res) {
 
     // Create postcode as a variable and ask the prototype to remember it, and look at the first two letters of the postcode
     var postcode = req.session.data['postcode'].substring(0,2).toUpperCase();
@@ -13,20 +13,20 @@ router.post('/crisis-support-finder/finder/postcode-search-branch', function (re
     // Check whether the postcode matches any of the below
     // Send user to next page based on postcode
     if (postcode == "LS"){
-      res.redirect('/crisis-support-finder/finder/results-west')
+      res.redirect('/crisis-support-finder/v2/finder/results-west')
     } else if(postcode == "EX") {
-        res.redirect('/crisis-support-finder/finder/results-south')
+        res.redirect('/crisis-support-finder/v2/finder/results-south')
     } else if(postcode == "PR") {
-        res.redirect('/crisis-support-finder/finder/results-north')
+        res.redirect('/crisis-support-finder/v2/finder/results-north')
     } else if(postcode == "CB") {
-        res.redirect('/crisis-support-finder/finder/results-london')
+        res.redirect('/crisis-support-finder/v2/finder/results-london')
     }
      else if(postcode == "NR") {
-    res.redirect('/crisis-support-finder/finder/results-east')
+    res.redirect('/crisis-support-finder/v2/finder/results-east')
 }
     else {
       // Send user to ineligible page if they use none of those postcodes
-      res.redirect('/crisis-support-finder/finder/results')
+      res.redirect('/crisis-support-finder/v2/finder/results')
     }
   
   })
