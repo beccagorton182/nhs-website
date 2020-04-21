6099cc712026fde05a1e88f8118c9cb1ca8a2e3c
beccagorton182 <rebecca.gorton1@nhs.net>
2020-04-21 14:09:11 +0100

    wales and scotland no results page

--- app/routes.js
+++ app/routes.js
@@ -10,35 +10,36 @@ router.post('/crisis-support-finder/v3/finder/postcode-search-branch', function 
     // Create postcode as a variable and ask the prototype to remember it, and look at the first two letters of the postcode
     var postcode = req.session.data['postcode'].substring(0,2).toUpperCase();
     var age = req.session.data['age'];
-  
-    // Check whether the postcode matches any of the below
-    // Send user to next page based on postcode
-    if (age >= 18)
-    {
-      //Check postcodes for over 18's
-      if (postcode == "LS"){
-        res.redirect('/crisis-support-finder/v3/finder/results-west')
-      } else if(postcode == "EX") {
-          res.redirect('/crisis-support-finder/v3/finder/results-south')
-      } else if(postcode == "PR") {
-          res.redirect('/crisis-support-finder/v3/finder/results-north')
-      } else if(postcode == "CB") {
-          res.redirect('/crisis-support-finder/v3/finder/results-london')
-      }
-      else if(postcode == "NR") {
-      res.redirect('/crisis-support-finder/v3/finder/results-east')
-      }
-      else {
-        // Send user to ineligible page if they use none of those postcodes
-        res.redirect('/crisis-support-finder/v3/finder/results')
-      }
-    } else
-    {
-      //Send to generic young person's page
-      res.redirect('/crisis-support-finder/v3/finder/results-young')
+
+  // Check whether the postcode matches any of the below
+  // Send user to next page based on postcode
+  if (age >= 18) {
+    //Check postcodes for over 18's
+    if (postcode == "LS") {
+      res.redirect('/crisis-support-finder/v3/finder/results-west')
+    } else if (postcode == "EX") {
+      res.redirect('/crisis-support-finder/v3/finder/results-south')
+    } else if (postcode == "PR") {
+      res.redirect('/crisis-support-finder/v3/finder/results-north')
+    } else if (postcode == "CB") {
+      res.redirect('/crisis-support-finder/v3/finder/results-london')
     }
-  
-  })
+    else if (postcode == "NR") {
+      res.redirect('/crisis-support-finder/v3/finder/results-east')
+    }
+    else if (postcode == "EH" || postcode == "CF") {
+      res.redirect('/crisis-support-finder/v3/finder/no-result')
+    }
+    else {
+      // Send user to ineligible page if they use none of those postcodes
+      res.redirect('/crisis-support-finder/v3/finder/results')
+    }
+  } else {
+    //Send to generic young person's page
+    res.redirect('/crisis-support-finder/v3/finder/results-young')
+  }
+
+})
 
 
 
