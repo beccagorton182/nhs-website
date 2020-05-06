1b9ec9a5c033d64bc3d7015b550c7134cfe9eaa6
beccagorton182 <rebecca.gorton1@nhs.net>
2020-05-06 15:06:29 +0100

    Version 4 first push
    Post-live user testing
    Experimental capacity and outside of england additions

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-// Branching example
+// Branching example -- version 3
 router.post('/crisis-support-finder/v3/finder/postcode-search-branch', function (req, res) {
 
     // Create postcode as a variable and ask the prototype to remember it, and look at the first two letters of the postcode
@@ -41,6 +41,42 @@ router.post('/crisis-support-finder/v3/finder/postcode-search-branch', function 
 
 })
 
+// Branching example -- version 4
+router.post('/crisis-support-finder/v4/finder/postcode-search-branch', function (req, res) {
+
+  // Create postcode as a variable and ask the prototype to remember it, and look at the first two letters of the postcode
+  var postcode = req.session.data['postcode'].substring(0,2).toUpperCase();
+  var age = req.session.data['age'];
+
+// Check whether the postcode matches any of the below
+// Send user to next page based on postcode
+if (age < 18) {
+  //Check postcodes for over 18's
+  if (postcode == "LS") {
+    res.redirect('/crisis-support-finder/v4/finder/results-west')
+  } else if (postcode == "BN") {
+    res.redirect('/crisis-support-finder/v4/finder/results-south')
+  } else if (postcode == "DH") {
+    res.redirect('/crisis-support-finder/v4/finder/results-north')
+  } else if (postcode == "SW") {
+    res.redirect('/crisis-support-finder/v4/finder/results-london')
+  }
+  else if (postcode == "NR") {
+    res.redirect('/crisis-support-finder/v4/finder/results-east')
+  }
+  else if (postcode == "EH" || postcode == "CF") {
+    res.redirect('/crisis-support-finder/v4/finder/no-result')
+  }
+  else {
+    // Send user to ineligible page if they use none of those postcodes
+    res.redirect('/crisis-support-finder/v4/finder/results')
+  }
+} else {
+  //Send to generic adult's page
+  res.redirect('/crisis-support-finder/v4/finder/results-adult')
+}
+
+})
 
 
 
