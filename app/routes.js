310037eea8c682f0ad3b5d7682ceea99180dc5aa
beccagorton182 <rebecca.gorton1@nhsnet>
2020-05-20 13:31:11 +0100

    MVP V4.1 push for testing
    Added various reassuring content and conditions pages to be tested
    Added the use my location button back in
    Created a 'multiple' results page

--- app/routes.js
+++ app/routes.js
@@ -45,13 +45,14 @@ router.post('/crisis-support-finder/v3/finder/postcode-search-branch', function 
 router.post('/crisis-support-finder/v4/finder/postcode-search-branch', function (req, res) {
 
   // Create postcode as a variable and ask the prototype to remember it, and look at the first two letters of the postcode
+  //Also store the age from the page and pass it through
   var postcode = req.session.data['postcode'].substring(0,2).toUpperCase();
   var age = req.session.data['age'];
 
 // Check whether the postcode matches any of the below
 // Send user to next page based on postcode
-if (age < 18) {
-  //Check postcodes for over 18's
+if (age > 18) {
+  //Check postcodes for under 18's
   if (postcode == "LS") {
     res.redirect('/crisis-support-finder/v4/finder/results-west')
   } else if (postcode == "BN") {
@@ -61,22 +62,24 @@ if (age < 18) {
   } else if (postcode == "SW") {
     res.redirect('/crisis-support-finder/v4/finder/results-london')
   }
-  else if (postcode == "NR") {
-    res.redirect('/crisis-support-finder/v4/finder/results-east')
+  else if (postcode == "PR") {
+    res.redirect('/crisis-support-finder/v4/finder/results-two')
   }
   else if (postcode == "EH" || postcode == "CF") {
     res.redirect('/crisis-support-finder/v4/finder/outside-england')
   }
   else {
-    // Send user to ineligible page if they use none of those postcodes
-    res.redirect('/crisis-support-finder/v4/finder/results')
+    // Send user to generic adult page
+    res.redirect('/crisis-support-finder/v4/finder/results-general')
   }
 } else {
-  //Send to generic adult's page
-  res.redirect('/crisis-support-finder/v4/finder/results-adult')
+  //Send to generic young person's page
+  res.redirect('/crisis-support-finder/v4/finder/results-young')
 }
 
 })
+
+
 
 
 
